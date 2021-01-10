eae84ebc398d778be23f4828869b73d8f658e883
layla-lili <laylaisa.211@gmail.com>
2021-01-10 21:58:43 +0300

    Updated conditional task with readline!

--- calculator.js
+++ calculator.js
@@ -1,18 +1,42 @@
 // Define the numbers and the operation here
-const firstNum=1;
-const secondNum=3;
-const operation='+';
-// Write if-statements here
+let firstNum=1;
+let secondNum=1;
+let operation='+';
 
-if(operation === '+'){
-   console.log(firstNum+secondNum);
-}
-   else if(operation === '-') {
-    console.log(firstNum-secondNum);
-   }
-   else if(operation === '*') {
-    console.log(firstNum*secondNum);
-   }
-   else {
-    console.log(firstNum/secondNum);
-   }
+
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout
+});
+
+rl.question("Enter first number ? ", function(firstNum) {
+   rl.question("Enter second number? ", function(secondNum) {
+      rl.question("Enter operation? ", function(operation) {
+  
+     firstNum=parseInt(firstNum,10);
+     secondNum=parseInt(secondNum,10);
+   // Write if-statements here
+         if(operation === '+') console.log(firstNum+secondNum);
+
+         else if(operation === '-') console.log(firstNum-secondNum);
+
+         else if(operation === '*') console.log(firstNum*secondNum);
+         else if(operation === '/') console.log(firstNum/secondNum);
+
+         else console.log("invlide operation");
+      
+       rl.close();
+   });
+});
+});
+//process.exit(0);
+
+
+
+
+
+
+   
